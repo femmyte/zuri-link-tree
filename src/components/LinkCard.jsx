446845de8a15f117de61204cf937fde8da476100
femmyte <sanyaoluadefemi@gmail.com>
2022-10-23 04:59:08 +0100

    Created a linkCard
    This serve as card for each row which can be called in the page
    this hold and render the title of each card

--- src/components/LinkCard.jsx
+++ src/components/LinkCard.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
-const LinkCard = () => {
-  return <div>LinkCard</div>;
+const LinkCard = ({ title, text }) => {
+  return (
+    <div className="card">
+      <h1 className="title">{title}</h1>
+      <p className="text">{text}</p>
+    </div>
+  );
 };
 
 export default LinkCard;
